69e4b07c0202e14ce2d83c19e22d50b403f99a30
alexnilsson <alexandernilsson12@gmail.com>
2018-04-04 13:47:53 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# python-image-scraper
-Searches and downloads images from the web based on specified keywords
+# gscraper
+A google image search scraper to automatically search and download images from the web
+
+## Install
+```
+pip install gscraper
+```
+
+## Format
+```python
+from gscraper import scrapeImages
+
+scrapeImages(search_queries='nicolas cage', limit=5, folder_name='<query-string>', output_directory='downloads', delay=0 )
+'''
+search_queries:     Comma separated <String> or <List>
+limit:              <Int> 0-100
+folder_name:        <String> or '<query-string>'
+output_directory:   <String>
+delay:              <Int> Seconds, delay between download queries
+
+'''
+
+```
+
+## Examples
+```python
+from gscraper import scrapeImages
+
+scrapeImages( search_queries='query', limit=3 ):
+'''
+downloads/
+| query/
+| | image-1
+| | image-2
+| | image-3
+'''
+scrapeImages( search_queries='query', limit=1, folder_name='output' ):
+'''
+downloads/
+| output/
+| | image-1
+'''
+
+scrapeImages( search_queries=['one','two'], limit=2, output_directory='numbers' ):
+'''
+numbers/
+| one/
+| | image-1
+| | image-2
+| two/
+| | image-1
+| | image-2
+'''
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+"""A google image search scraper to automatically search and download images from the web
+See
+https://github.com/AlexNilsson/python-image-scraper
+"""
+
+# Developers:
+# Install with:
+#   pip install -e .
+# Distribute with:
+# python setup.py bdist_wheel
+# twine upload dist/*
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='gscraper',
+    version='0.1.1',
+    packages=find_packages(),
+
+    install_requires=['setuptools>=28.8.0'],
+
+    author='Alexander Nilsson',
+    author_email='contact@alexnilsson.io',
+    description='Automatically download images from google image search',
+    license="MIT",
+    keywords='web scraper google image images search automatic download',
+    url='https://github.com/AlexNilsson/python-image-scraper',
+
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+)
